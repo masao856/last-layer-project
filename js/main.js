dc3a7117caea75d775d8d00eb8eb397cea786736
masao856 <l122949r@iCloud.com>
2025-04-16 13:19:03 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 
-import { logMessage, showSkillSelection } from './ui.js';
+import { logMessage } from './ui.js';
 
 document.getElementById("move").addEventListener("click", () => logMessage("進んだ！"));
 document.getElementById("back").addEventListener("click", () => logMessage("戻った！"));
 document.getElementById("attack").addEventListener("click", () => logMessage("攻撃した！"));
-document.getElementById("item").addEventListener("click", () => logMessage("アイテムはまだ使えません"));
-document.getElementById("skill").addEventListener("click", () => showSkillSelection());
+document.getElementById("item").addEventListener("click", () => logMessage("アイテムボタンが押された！"));
+document.getElementById("skill").addEventListener("click", () => logMessage("スキルボタンが押された！"));
--- js/ui.js
+++ js/ui.js
@@ -1,16 +1,4 @@
 
-import { player } from './state.js';
-import { skills } from './skills.js';
-import { castSkill } from './battle.js';
-
-export function logMessage(msg) {
-  document.getElementById("log").innerText = msg;
-}
-
-export function showSkillSelection() {
-  let skillList = player.skills.map(name => {
-    const s = skills[name];
-    return `<button onclick="window.castSkillByName('${s.name}')">${s.name} (${s.cost}MP)</button>`;
-  }).join("<br>");
-  document.getElementById("log").innerHTML = "<b>スキルを選択:</b><br>" + skillList;
+export function logMessage(message) {
+  document.getElementById("log").innerText = message;
 }
