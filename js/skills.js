5c31f7d78b86f4ac8a9857d8f08ac33cec328885
masao856 <l122949r@iCloud.com>
2025-04-16 13:16:44 +0900

    Add files via upload

--- js/skills.js
+++ js/skills.js
@@ -1,9 +1,57 @@
 
-export const skills = {
-  "ファイアボール": { name: "ファイアボール", type: "attack", power: 15, cost: 5, attribute: "火", effect: "単体魔法攻撃" },
-  "ウォータースピア": { name: "ウォータースピア", type: "attack", power: 14, cost: 5, attribute: "水", effect: "必中魔法攻撃" },
-  "サンダー": { name: "サンダー", type: "attack", power: 13, cost: 5, attribute: "雷", effect: "まれに麻痺" },
-  "ヒール": { name: "ヒール", type: "heal", power: 20, cost: 4, attribute: "光", effect: "HP回復" },
-  "シャドウボルト": { name: "シャドウボルト", type: "attack", power: 15, cost: 5, attribute: "闇", effect: "闇属性魔法" },
-  "ポイズン": { name: "ポイズン", type: "status", power: 0, cost: 3, attribute: "毒", effect: "継続ダメージ" }
-};
+export const skills = [
+  {
+    id: "fireball",
+    name: "ファイアボール",
+    mpCost: 4,
+    type: "attack",
+    power: 15,
+    attribute: "火",
+    description: "敵1体に火属性の魔法攻撃"
+  },
+  {
+    id: "waterspear",
+    name: "ウォータースピア",
+    mpCost: 4,
+    type: "attack",
+    power: 14,
+    attribute: "水",
+    description: "敵1体に水属性の必中魔法攻撃"
+  },
+  {
+    id: "thunder",
+    name: "サンダー",
+    mpCost: 5,
+    type: "attack",
+    power: 13,
+    attribute: "雷",
+    description: "敵1体に雷属性の魔法攻撃（まれに麻痺）"
+  },
+  {
+    id: "heal",
+    name: "ヒール",
+    mpCost: 3,
+    type: "heal",
+    power: 20,
+    attribute: "光",
+    description: "味方1体のHPを回復"
+  },
+  {
+    id: "shadowbolt",
+    name: "シャドウボルト",
+    mpCost: 4,
+    type: "attack",
+    power: 15,
+    attribute: "闇",
+    description: "敵1体に闇属性の魔法攻撃"
+  },
+  {
+    id: "poison",
+    name: "ポイズン",
+    mpCost: 3,
+    type: "status",
+    power: 0,
+    attribute: "毒",
+    description: "敵を毒状態にする"
+  }
+];
