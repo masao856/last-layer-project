51bf74abaca198cbd201cca52728257ee3fa2f1b
masao856 <l122949r@iCloud.com>
2025-04-16 18:00:56 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,1 +1,19 @@
-// メイン処理のダミー（中身は省略）
+function log(msg) {
+  document.getElementById("log").innerText = msg;
+}
+
+document.getElementById("move").addEventListener("click", () => {
+  log("進んだ！");
+});
+document.getElementById("back").addEventListener("click", () => {
+  log("戻った！");
+});
+document.getElementById("attack").addEventListener("click", () => {
+  log("攻撃した！");
+});
+document.getElementById("item").addEventListener("click", () => {
+  log("アイテムを使った（仮）");
+});
+document.getElementById("skill").addEventListener("click", () => {
+  log("スキルを使った（仮）");
+});
