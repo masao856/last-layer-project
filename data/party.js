8c216872febae69791eb60fadc34ee0e560bdb34
masao856 <l122949r@iCloud.com>
2025-04-17 12:40:34 +0900

    Add files via upload

---
+++ data/party.js
@@ -0,0 +1,56 @@
+const party = [
+  {
+    "id": "hero",
+    "name": "勇者",
+    "class": "勇者",
+    "maxHp": 100,
+    "hp": 100,
+    "attack": 15,
+    "skills": [
+      "ファイアボール",
+      "ヒール"
+    ],
+    "equipment": {
+      "rightHand": "short_sword",
+      "leftHand": null,
+      "armor": "armor_black_iron",
+      "shield": null
+    }
+  },
+  {
+    "id": "warrior",
+    "name": "戦士",
+    "class": "戦士",
+    "maxHp": 120,
+    "hp": 120,
+    "attack": 18,
+    "skills": [
+      "エアスラッシュ"
+    ],
+    "equipment": {
+      "rightHand": "bastard_sword",
+      "leftHand": null,
+      "armor": "armor_steel",
+      "shield": "iron_shield"
+    }
+  },
+  {
+    "id": "mage",
+    "name": "魔法使い",
+    "class": "魔法使い",
+    "maxHp": 80,
+    "hp": 80,
+    "attack": 10,
+    "skills": [
+      "サンダー",
+      "ヒール",
+      "ポイズン"
+    ],
+    "equipment": {
+      "rightHand": "fire_staff",
+      "leftHand": null,
+      "armor": "armor_cultist",
+      "shield": null
+    }
+  }
+];
