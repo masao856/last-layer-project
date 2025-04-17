75d76b7d26cdc9fa25ce91776f4d6d0eb5efc53d
masao856 <l122949r@iCloud.com>
2025-04-17 12:45:02 +0900

    Add files via upload

--- data/enemies.js
+++ data/enemies.js
@@ -2,390 +2,185 @@ const enemies = [
   {
     "id": "shadow",
     "name": "影",
-    "layer": 1,
-    "hp": 70,
-    "atk": 20,
-    "def": 10,
-    "type": "霊体",
-    "element": "dark",
-    "image": "shadow.png",
-    "skills": [
-      "シャドウボルト"
-    ],
-    "drops": [
-      {
-        "item": "MPポーション",
-        "rate": 0.2
-      }
-    ]
+    "hp": 40,
+    "attack": 8,
+    "skills": [],
+    "aiType": "aggressive"
   },
   {
     "id": "bandit",
     "name": "野盗",
-    "layer": 1,
-    "hp": 60,
-    "atk": 22,
-    "def": 8,
-    "type": "人間",
-    "element": "null",
-    "image": "bandit.png",
-    "skills": [],
-    "drops": [
-      {
-        "item": "食糧",
-        "rate": 0.2
-      },
-      {
-        "item": "ポーション",
-        "rate": 0.3
-      }
-    ]
+    "hp": 50,
+    "attack": 10,
+    "skills": [
+      "ポイズン"
+    ],
+    "aiType": "tricky"
   },
   {
     "id": "beastman_bandit",
     "name": "獣人の野盗",
-    "layer": 1,
-    "hp": 85,
-    "atk": 25,
-    "def": 12,
-    "type": "獣人",
-    "element": "null",
-    "image": "beastman_bandit.png",
-    "skills": [],
-    "drops": [
-      {
-        "item": "食糧",
-        "rate": 0.25
-      }
-    ]
+    "hp": 60,
+    "attack": 12,
+    "skills": [
+      "ファイアボール"
+    ],
+    "aiType": "caster"
   },
   {
     "id": "zombie",
     "name": "ゾンビ",
-    "layer": 1,
-    "hp": 90,
-    "atk": 18,
-    "def": 10,
-    "type": "アンデッド",
-    "element": "poison",
-    "image": "zombie.png",
+    "hp": 70,
+    "attack": 9,
+    "skills": [],
+    "aiType": "aggressive"
+  },
+  {
+    "id": "armor_knight",
+    "name": "黒鉄の鎧",
+    "hp": 120,
+    "attack": 14,
     "skills": [
+      "ダークネスウェーブ"
+    ],
+    "aiType": "defensive"
+  },
+  {
+    "id": "fallen_king",
+    "name": "堕ちた王",
+    "hp": 200,
+    "attack": 20,
+    "skills": [
+      "ダークネスウェーブ",
       "ポイズン"
     ],
-    "drops": [
-      {
-        "item": "朽ちた布",
-        "rate": 0.3
-      }
-    ]
+    "aiType": "boss_pattern1"
   },
   {
     "id": "gravekeeper_wraith",
     "name": "墓守の怨霊",
-    "layer": 2,
-    "hp": 400,
-    "atk": 65,
-    "def": 30,
-    "type": "怨霊ボス",
-    "element": "dark",
-    "image": "gravekeeper_wraith.png",
-    "isBoss": true,
+    "hp": 180,
+    "attack": 18,
     "skills": [
-      "シャドウボルト",
-      "ドレイン",
-      "ファイアボール"
+      "ダークネスウェーブ"
     ],
-    "drops": [
-      {
-        "item": "怨霊の斧",
-        "rate": 0.1
-      },
-      {
-        "item": "MPポーション",
-        "rate": 0.5
-      }
-    ]
+    "aiType": "boss_pattern1"
   },
   {
     "id": "cursed_armor_screaming",
-    "name": "呪われた叫びの鎧",
-    "layer": 2,
-    "hp": 260,
-    "atk": 48,
-    "def": 28,
-    "type": "呪鎧・中ボス",
-    "element": "dark",
-    "image": "cursed_armor_screaming.png",
-    "isMidBoss": true,
-    "skills": [
-      "シャドウボルト",
-      "咆哮"
-    ],
-    "drops": [
-      {
-        "item": "呪いの破片",
-        "rate": 0.2
-      },
-      {
-        "item": "ポーション",
-        "rate": 0.4
-      }
-    ]
-  },
-  {
-    "id": "ghost_grave_robber",
-    "name": "墓泥棒の亡霊",
-    "layer": 2,
-    "hp": 100,
-    "atk": 25,
-    "def": 12,
-    "type": "霊体",
-    "element": "null",
-    "image": "ghost_grave_robber.png",
-    "skills": [],
-    "drops": [
-      {
-        "item": "ポーション",
-        "rate": 0.3
-      }
-    ]
-  },
-  {
-    "id": "enemy_mummy",
-    "name": "古代のミイラ",
-    "layer": 2,
-    "hp": 130,
-    "atk": 32,
-    "def": 16,
-    "type": "アンデッド",
-    "element": "null",
-    "image": "enemy_mummy.png",
-    "skills": [],
-    "drops": [
-      {
-        "item": "乾いた包帯",
-        "rate": 0.4
-      }
-    ]
-  },
-  {
-    "id": "enemy_floating_soul",
-    "name": "彷徨う魂",
-    "layer": 2,
-    "hp": 90,
-    "atk": 24,
-    "def": 10,
-    "type": "霊体",
-    "element": "dark",
-    "image": "enemy_floating_soul.png",
-    "skills": [
-      "ドレイン"
-    ],
-    "drops": [
-      {
-        "item": "MPポーション",
-        "rate": 0.2
-      }
-    ]
-  },
-  {
-    "id": "cursed_shadow",
-    "name": "呪影",
-    "layer": 2,
-    "hp": 110,
-    "atk": 30,
-    "def": 14,
-    "type": "呪霊",
-    "element": "dark",
-    "image": "cursed_shadow.png",
-    "skills": [
-      "シャドウボルト"
-    ],
-    "drops": [
-      {
-        "item": "呪いの粉",
-        "rate": 0.25
-      }
-    ]
-  },
-  {
-    "id": "skeleton_soldier",
-    "name": "スケルトンソルジャー",
-    "layer": 2,
+    "name": "叫びの鎧",
     "hp": 120,
-    "atk": 35,
-    "def": 18,
-    "type": "アンデッド",
-    "element": "null",
-    "image": "skeleton_soldier.png",
-    "skills": [],
-    "drops": [
-      {
-        "item": "骨のかけら",
-        "rate": 0.3
-      }
-    ]
-  },
-  {
-    "id": "sand_snake",
-    "name": "サンドスネーク",
-    "layer": 3,
-    "hp": 160,
-    "atk": 38,
-    "def": 20,
-    "type": "魔獣",
-    "element": "poison",
-    "image": "sand_snake.png",
-    "skills": [
-      "ポイズン"
-    ],
-    "drops": [
-      {
-        "item": "毒の牙",
-        "rate": 0.3
-      }
-    ]
-  },
-  {
-    "id": "sunamiri_no_mouja",
-    "name": "砂霧の亡者",
-    "layer": 3,
-    "hp": 190,
-    "atk": 42,
-    "def": 22,
-    "type": "アンデッド戦士",
-    "element": "null",
-    "image": "sunamiri_no_mouja.png",
-    "skills": [],
-    "drops": [
-      {
-        "item": "サビた剣",
-        "rate": 0.2
-      }
-    ]
-  },
-  {
-    "id": "iseki_no_saikutsusha",
-    "name": "遺跡の採掘者",
-    "layer": 3,
-    "hp": 150,
-    "atk": 45,
-    "def": 15,
-    "type": "人間",
-    "element": "null",
-    "image": "iseki_no_saikutsusha.png",
-    "skills": [
-      "クロスボウショット"
-    ],
-    "drops": [
-      {
-        "item": "食糧",
-        "rate": 0.2
-      },
-      {
-        "item": "クロスボウ",
-        "rate": 0.1
-      }
-    ]
-  },
-  {
-    "id": "lizardman_sand",
-    "name": "熱砂のリザードマン",
-    "layer": 3,
-    "hp": 200,
-    "atk": 50,
-    "def": 25,
-    "type": "爬虫人",
-    "element": "fire",
-    "image": "lizardman_sand.png",
+    "attack": 16,
     "skills": [
       "ファイアボール"
     ],
-    "drops": [
-      {
-        "item": "リザードの皮",
-        "rate": 0.4
-      }
-    ]
+    "aiType": "defensive"
   },
   {
-    "id": "karakara_mummy",
-    "name": "カラカラミイラ",
-    "layer": 3,
-    "hp": 180,
-    "atk": 40,
-    "def": 24,
-    "type": "アンデッド",
-    "element": "null",
-    "image": "karakara_mummy.png",
+    "id": "ghost_grave_robber",
+    "name": "幽霊盗掘者",
+    "hp": 60,
+    "attack": 9,
     "skills": [
-      "乾きの呪い"
-    ],
-    "drops": [
-      {
-        "item": "乾いた包帯",
-        "rate": 0.4
-      }
-    ]
-  },
-  {
-    "id": "sajin_no_majutsushi",
-    "name": "砂塵の魔術師",
-    "layer": 3,
-    "hp": 280,
-    "atk": 55,
-    "def": 26,
-    "type": "中ボス・魔術師",
-    "element": "fire",
-    "image": "sajin_no_majutsushi.png",
-    "isMidBoss": true,
-    "skills": [
-      "ファイアボール",
-      "サンダー",
-      "シャドウボルト"
-    ],
-    "drops": [
-      {
-        "item": "魔術師のローブ",
-        "rate": 0.1
-      },
-      {
-        "item": "MPポーション",
-        "rate": 0.5
-      }
-    ]
-  },
-  {
-    "id": "zanadu",
-    "name": "幻王ザナドゥ",
-    "layer": 3,
-    "hp": 500,
-    "atk": 70,
-    "def": 35,
-    "type": "ボス・魔王",
-    "element": "fire",
-    "image": "zanadu.png",
-    "isBoss": true,
-    "skills": [
-      "ファイアボール",
-      "ヒール",
-      "ドレイン",
-      "サンダー",
       "ポイズン"
     ],
-    "drops": [
-      {
-        "item": "ザナドゥの宝珠",
-        "rate": 0.05
-      },
-      {
-        "item": "魔王のマント",
-        "rate": 0.1
-      },
-      {
-        "item": "エーテル",
-        "rate": 0.5
-      }
-    ]
+    "aiType": "tricky"
+  },
+  {
+    "id": "enemy_mummy",
+    "name": "古代ミイラ",
+    "hp": 80,
+    "attack": 10,
+    "skills": [],
+    "aiType": "aggressive"
+  },
+  {
+    "id": "enemy_floating_soul",
+    "name": "さまよう魂",
+    "hp": 50,
+    "attack": 7,
+    "skills": [
+      "シャドウボルト"
+    ],
+    "aiType": "caster"
+  },
+  {
+    "id": "cursed_shadow",
+    "name": "呪われし影",
+    "hp": 55,
+    "attack": 8,
+    "skills": [
+      "ポイズン"
+    ],
+    "aiType": "tricky"
+  },
+  {
+    "id": "skeleton_soldier",
+    "name": "スケルトン兵士",
+    "hp": 65,
+    "attack": 11,
+    "skills": [],
+    "aiType": "aggressive"
+  },
+  {
+    "id": "sand_wraith",
+    "name": "砂霧の亡者",
+    "hp": 75,
+    "attack": 11,
+    "skills": [
+      "ポイズン"
+    ],
+    "aiType": "tricky"
+  },
+  {
+    "id": "ruin_raider",
+    "name": "遺跡の採掘者",
+    "hp": 85,
+    "attack": 13,
+    "skills": [
+      "サンダー"
+    ],
+    "aiType": "caster"
+  },
+  {
+    "id": "sand_lizardman",
+    "name": "熱砂のリザードマン",
+    "hp": 90,
+    "attack": 14,
+    "skills": [],
+    "aiType": "aggressive"
+  },
+  {
+    "id": "dried_mummy",
+    "name": "カラカラミイラ",
+    "hp": 100,
+    "attack": 12,
+    "skills": [
+      "ファイアボール"
+    ],
+    "aiType": "defensive"
+  },
+  {
+    "id": "desert_phantom",
+    "name": "砂塵の魔術師",
+    "hp": 140,
+    "attack": 16,
+    "skills": [
+      "シャドウボルト",
+      "ダークネスウェーブ"
+    ],
+    "aiType": "boss_pattern1"
+  },
+  {
+    "id": "zanadu_king",
+    "name": "幻王ザナドゥ",
+    "hp": 240,
+    "attack": 22,
+    "skills": [
+      "ダークネスウェーブ",
+      "ポイズン",
+      "サンダーストーム"
+    ],
+    "aiType": "boss_pattern1"
   }
 ];
